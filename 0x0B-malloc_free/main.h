53cf8fe0338ba9a3cbec61269fe425289f6696d3
ahmed osama <ahmad.osama.elfouly@gmail.com>
2023-04-11 09:02:07 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 char *creat_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
-char *_str_concat(char *s2, char *s2);
+char *_str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
