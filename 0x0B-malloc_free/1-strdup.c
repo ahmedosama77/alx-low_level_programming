6f67a1989708a3daadaeda96788e278fcf518e45
ahmed osama <ahmad.osama.elfouly@gmail.com>
2023-04-11 09:16:14 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * _strup - duplicate to new memoty space location
+ * _strdup - duplicate to new memory space location
  * @str: char
  * Return: 0
  */
