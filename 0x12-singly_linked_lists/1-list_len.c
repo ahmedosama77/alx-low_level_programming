2269db87cc3f14bcc415e344380af0dbe653f984
Ahmed osama <ahmed.osama.elfouly@gmail.com>
2023-04-28 09:40:46 -0700

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,18 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * @h: pointer to first node
  *
- * Return: number of elements in h
+ * Return: size of list
  */
 size_t list_len(const list_t *h)
 {
-size_t n = 0;
+size_t i = 0;
 
 	while (h)
 	{
-		n++;
 		h = h->next;
+		i++;
 	}
-	return (n);
+	return (i);
 }
