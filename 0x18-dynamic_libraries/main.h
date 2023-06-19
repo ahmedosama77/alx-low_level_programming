79f44af128ca149470d179d5ce70a4fd5df9c8b4
= <=>
2023-06-19 16:25:57 -0700

    updated

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DYN_LIBS
+#define DYN_LIBS
 
 int _putchar(char c);
 int _islower(int c);
@@ -13,7 +10,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
